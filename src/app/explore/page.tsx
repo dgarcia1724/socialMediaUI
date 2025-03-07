47e940739ba217aaa67075363748708e49744641
Danny Garcia <dgarcia1724@berkeley.edu>
2025-03-06 20:53:50 -0800

    Added pages

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,7 @@
+export default function Explore() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Explore</h1>
+    </div>
+  );
+}
